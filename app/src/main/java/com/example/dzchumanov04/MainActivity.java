a09577a8ac8c55e44441adb7d3c4c12837e6fe32
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-10-06 13:26:49 +0300

    Simple version of homework 1 done: one additional string added. The strings described in both layout and corresponding strings.xml files. onClick functionality of hello string described. I will add something else if I have time.

--- app/src/main/java/com/example/dzchumanov04/MainActivity.java
+++ app/src/main/java/com/example/dzchumanov04/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dzchumanov04;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+/**
+ * При нажатии устанавливает размер текста в 40dp и меняет текст.
+ * При смене языка в настройках, текст и его размер сбросятся до дефолтных.
+ * При смене размера шрифта в настройках, текст меняет дефолтный размер
+*/
+ public void helloOnClick(View view) {
+        TextView textHello = findViewById(R.id.text_hello);
+        textHello.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
+        if (textHello.getText().toString().equals("Hello!"))
+            textHello.setText("Привет!");
+        else
+            textHello.setText("Hello!");
     }
 }
