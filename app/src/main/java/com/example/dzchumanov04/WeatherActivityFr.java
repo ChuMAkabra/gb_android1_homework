63851cc007c143711c0cce8aae59b04c440b6620
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-11-13 20:51:07 +0300

    Added menu with icons in the upper toolbar. Starting adding functionality for these buttons (new "About" fragment added). Separator added for night mode. Main App Theme now has "Theme.MaterialComponents.DayNight" parent. Min SDK changed to 26 to be in line with DateTime features. Gradle plugin updated.

--- app/src/main/java/com/example/dzchumanov04/WeatherActivityFr.java
+++ app/src/main/java/com/example/dzchumanov04/WeatherActivityFr.java
@@ -2,8 +2,12 @@ package com.example.dzchumanov04;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class WeatherActivityFr extends AbstractActivity {
     @Override
@@ -24,5 +28,44 @@ public class WeatherActivityFr extends AbstractActivity {
 //                    .replace(R.id.frWeather, new FragmentWeather())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        /**
+         * Система пишет: "Resource IDs will be non-final in Android Gradle Plugin version 5.0, avoid using them in switch case statements"
+         * Не совсем  понял причины. Так не стоит все же?
+         */
+        //TODO замени тост переходом на соответствующую страницу
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(), getString(R.string.settings), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.about:
+                Toast.makeText(this, getString(R.string.about), Toast.LENGTH_SHORT).show();
+                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frContainer, new FragmentCity())
+                            .commit();
+                } else {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frWeather, new FragmentCity())
+                            .commit();
+                }
+
+
+                return true;
+        }
+        return true;
     }
 }
